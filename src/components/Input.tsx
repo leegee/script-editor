9dfaab53da8004e6da053e6ffc6ecac213d0bf0e
Lee Goddard <leegee@gmail.com>
2025-06-30 11:36:08 +0200

    Mark when empty

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,12 +9,20 @@ interface TextInputProps extends Omit<JSX.InputHTMLAttributes<HTMLInputElement |
 const TextInput: Component<TextInputProps> = (props) => {
     const { as = 'input', value, ...rest } = props;
 
+    const val = typeof value === 'function' ? value() : undefined;
+
+    if (
+        typeof val === 'undefined' ||
+        (typeof val !== 'number' && val.length === 0)
+    ) {
+        rest.class = (rest.class || '') + ' empty';
+    }
+
     const rv = as === 'textarea'
-        ? <textarea value={props.value()} {...(rest as JSX.TextareaHTMLAttributes<HTMLTextAreaElement>)} />
-        : <input value={props.value()} {...(rest as JSX.InputHTMLAttributes<HTMLInputElement>)} />;
+        ? <textarea value={val} {...(rest as JSX.TextareaHTMLAttributes<HTMLTextAreaElement>)} />
+        : <input value={val} {...(rest as JSX.InputHTMLAttributes<HTMLInputElement>)} />;
 
     return rv;
 };
-
 
 export default TextInput;
