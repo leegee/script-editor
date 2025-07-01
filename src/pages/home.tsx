37909329996922738ba80275644322bd015abc92
Lee Goddard <leegee@gmail.com>
2025-07-01 19:12:29 +0200

    hide panels

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,16 +4,25 @@ import LocationList from '../components/lists/LocationList';
 import Card from '../components/cards/Card';
 import CharacterList from '../components/lists/CharacterList';
 import ActCreator from '../components/creators/ActCreator';
+import Switch from '../components/Switch';
+import { uiOptions, setUiOptions } from '../stores/ui';
+import { Show } from 'solid-js';
 
 export default function Home(props) {
   return (
     <main class="home-layout">
       <aside class="panel">
-        <h2>&nbsp;</h2>
-        <Card class="act-panel" title="Acts" open={true}>
-          <ActsList />
-          <ActCreator />
-        </Card>
+        <header>
+          <span>
+            Show <Switch checked={uiOptions.showLeftSidePanel} onUpdate={(checked) => setUiOptions('showLeftSidePanel', checked)} />
+          </span>
+        </header>
+        <Show when={uiOptions.showLeftSidePanel}>
+          <Card class="act-panel" title="Acts" open={true}>
+            <ActsList />
+            <ActCreator />
+          </Card>
+        </Show>
       </aside>
 
       <article class="main-content">
@@ -21,14 +30,20 @@ export default function Home(props) {
       </article>
 
       <aside class="panel">
-        <h2>&nbsp;</h2>
-        <Card class="character-panel" title="Characters" open={true}>
-          <CharacterList />
-        </Card>
+        <header>
+          <span>
+            Show <Switch checked={uiOptions.showRightSidePanel} onUpdate={(checked) => setUiOptions('showRightSidePanel', checked)} />
+          </span>
+        </header>
+        <Show when={uiOptions.showRightSidePanel}>
+          <Card class="character-panel" title="Characters" open={true}>
+            <CharacterList />
+          </Card>
 
-        <Card class="location-panel" title="Location" open={true}>
-          <LocationList />
-        </Card>
+          <Card class="location-panel" title="Location" open={true}>
+            <LocationList />
+          </Card>
+        </Show>
       </aside>
 
     </main>
--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -3,11 +3,15 @@ import { createStore } from "solid-js/store";
 type UiOptionsType = {
     showActMetaData: boolean;
     showSceneData: boolean;
+    showLeftSidePanel: boolean;
+    showRightSidePanel: boolean;
 };
 
 const defaults: UiOptionsType = {
     showActMetaData: false,
     showSceneData: false,
+    showLeftSidePanel: true,
+    showRightSidePanel: true,
 };
 
 export const [uiOptions, setUiOptions] = createStore<UiOptionsType>(defaults);
