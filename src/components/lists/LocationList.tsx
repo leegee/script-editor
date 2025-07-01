eca1b7bbac32b64c583af5c6152d1f1117546162
Lee Goddard <leegee@gmail.com>
2025-07-01 15:20:16 +0200

    remove fossils

--- src/components/lists/LocationList.tsx
+++ src/components/lists/LocationList.tsx
@@ -1,26 +1,23 @@
 import { For, Show, createMemo } from 'solid-js';
-import type { Location } from '../../lib/types';
 import LocationCard from '../cards/LocationCard';
 import { storyApi } from '../../lib/story';
 import LocationCreator from '../creators/LocationCreator';
 
-type LocationsListProps = {
-    locations?: Location[];
-};
-
-export default (props: LocationsListProps) => {
-    const locations = createMemo(() => props.locations ?? storyApi.getLocations());
+export default () => {
+    const locations = createMemo(() => storyApi.getLocations());
 
     return (
-        <Show when={locations()} fallback={<div>No lcations found</div>}>
-            <section class="location-list" role="list" aria-label="Locations List">
-                <For each={locations()}>
-                    {(location) => (
-                        <LocationCard location={location} summary={true} />
-                    )}
-                </For>
-                <LocationCreator />
-            </section>
-        </Show>
+        <section>
+            <Show when={locations()} fallback={<div>No lcations found</div>}>
+                <section class="location-list" role="list" aria-label="Locations List">
+                    <For each={locations()}>
+                        {(location) => (
+                            <LocationCard location={location} summary={true} />
+                        )}
+                    </For>
+                    <LocationCreator />
+                </section>
+            </Show>
+        </section>
     );
 };
