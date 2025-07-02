5f6de68abfa7f7c1de01cfcc0914ece475e09199
Lee Goddard <leegee@gmail.com>
2025-07-02 13:55:08 +0200

    Hide panels not controls

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,41 +12,40 @@ import LocationCreator from '../components/creators/LocationCreator';
 
 export default function Home(props) {
   return (
-    <main class="home-layout">
-      <aside class="panel">
-        <header>
-          <span>
-            <Switch checked={uiOptions.showLeftSidePanel} onUpdate={(checked) => setUiOptions('showLeftSidePanel', checked)} />
-          </span>
-        </header>
+    <>
+      <header class="home-layout-controls">
+        <span>
+          <Switch checked={uiOptions.showLeftSidePanel} onUpdate={(checked) => setUiOptions('showLeftSidePanel', checked)} />
+
+          <Switch checked={uiOptions.showRightSidePanel} onUpdate={(checked) => setUiOptions('showRightSidePanel', checked)} />
+        </span>
+      </header>
+
+      <main class="home-layout">
         <Show when={uiOptions.showLeftSidePanel}>
-          <Card class="act-panel" title="Acts" open={true} menuItems={<ActCreator />}>
-            <ActsList />
-          </Card>
+          <aside class="panel">
+            <Card class="act-panel" title="Acts" open={true} menuItems={<ActCreator />}>
+              <ActsList />
+            </Card>
+          </aside>
         </Show>
-      </aside>
 
-      <article class="main-content">
-        {props.children}
-      </article>
+        <article class="main-content">
+          {props.children}
+        </article>
 
-      <aside class="panel right">
-        <header>
-          <span>
-            <Switch checked={uiOptions.showRightSidePanel} onUpdate={(checked) => setUiOptions('showRightSidePanel', checked)} />
-          </span>
-        </header>
         <Show when={uiOptions.showRightSidePanel}>
-          <Card class="character-panel" title="Characters" open={true} menuItems={<CharacterCreator />}>
-            <CharacterList />
-          </Card>
+          <aside class="panel right">
+            <Card class="character-panel" title="Characters" open={true} menuItems={<CharacterCreator />}>
+              <CharacterList />
+            </Card>
 
-          <Card class="location-panel" title="Location" open={true} menuItems={<LocationCreator />}>
-            <LocationList />
-          </Card>
+            <Card class="location-panel" title="Location" open={true} menuItems={<LocationCreator />}>
+              <LocationList />
+            </Card>
+          </aside>
         </Show>
-      </aside>
-
-    </main>
+      </main>
+    </>
   );
 }
