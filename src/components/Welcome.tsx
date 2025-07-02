7a2e9298e573c7491596ec4e4113de9c1b1b27f3
Lee Goddard <leegee@gmail.com>
2025-07-02 18:10:52 +0200

    blurb

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,6 +2,11 @@ export default () => {
     return (
         <main>
             <h1>Welcome</h1>
+            <p>
+                This is an experimental script-writing application
+                that stores the script in the browser, as well as
+                allowing it to be stored to, and loaded from, file.
+            </p>
         </main>
     );
 }
