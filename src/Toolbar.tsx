273b9714fcbc91ae37da8699a47a6207bfc712c7
Lee Goddard <leegee@gmail.com>
2025-07-04 09:35:57 +0200

    var name

--- src/Toolbar.tsx
+++ src/Toolbar.tsx
@@ -20,7 +20,7 @@ export default function () {
             </li>
             <li>
                 <label>Scene Info</label>
-                <Switch checked={uiOptions.showSceneMetaData} onUpdate={(checked) => setUiOptions('showSceneData', checked)} />
+                <Switch checked={uiOptions.showSceneMetaData} onUpdate={(checked) => setUiOptions('showSceneMetaData', checked)} />
             </li>
             <li class='spacer'></li>
             <li>
